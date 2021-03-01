569705d1f17a922e48c29ba04703e56b9596dda4
Kevin Noviski <Oem@OEM-PC>
2021-02-28 21:59:30 -0300

    SignIn.RequireConfirmedAccount = false

--- ActivitiesManager/Data/Migrations/20210301002105_InitialCreate.Designer.cs
+++ ActivitiesManager/Data/Migrations/20210301002105_InitialCreate.Designer.cs
@@ -4,14 +4,16 @@ using ActivitiesManager.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ActivitiesManager.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210301002105_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ActivitiesManager/Models/TodoItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActivitiesManager.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime DT_Creation { get; set; }
+
+        [Required]
+        public DateTime DT_DeadLine { get; set; }
+
+
+        public DateTime? DT_Done { get; set; }
+
+        [Required]
+        public int Priority { get; set; }
+
+    }
+}
